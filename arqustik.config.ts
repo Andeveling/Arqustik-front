5393c1da032ff3f6c60c9695f64e8ccec1a1cd83
Andres Parra <andeveling@gmail.com>
2022-12-09 17:59:42 -0500

    fix/host links

--- arqustik.config.ts
+++ arqustik.config.ts
@@ -1,12 +1,12 @@
-const dev = process.env.NODE_ENV !== "production"
+const dev = process.env.NODE_ENV !== 'production'
 
 export const arqustikConfig = {
-  STRAPI_SERVER: dev ? "http://localhost:1337/api" : "https://your_deployment.server.com",
-  NEXT_SERVER: dev ? "http://localhost:3000/api" : "https://your_deployment.server.com",
+  STRAPI_SERVER: dev ? 'http://localhost:1337/api' : 'https://arqustik-back-production.up.railway.app/api',
+  NEXT_SERVER: dev ? 'http://localhost:3000/api' : 'https://arqustikquoter.vercel.app/',
 }
 
 export const endpoints = {
-  quotations: "/quotations",
-  clients: "/clients",
-  windows: "/windows",
+  quotations: '/quotations',
+  clients: '/clients',
+  windows: '/windows',
 }
--- components/TopArea.tsx
+++ components/TopArea.tsx
@@ -1,13 +1,11 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import { ArrowRightOnRectangleIcon, ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid"
-import { Menu } from "@headlessui/react"
-import { Avatar, Button, Dropdown } from "flowbite-react"
-import { HomeIcon, WrenchIcon, Cog6ToothIcon, ArrowLeftIcon } from "@heroicons/react/24/solid"
-import QuoterMenu from "./QuoterMenu"
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid'
+import { Button } from 'flowbite-react'
+import { signIn, useSession } from 'next-auth/react'
+import QuoterMenu from './QuoterMenu'
 
 export default function TopArea() {
   const { data: session, status } = useSession()
-  if (status === "loading") return null
+  if (status === 'loading') return null
   if (session) {
     return <QuoterMenu />
   } else {
@@ -18,22 +16,3 @@ export default function TopArea() {
     )
   }
 }
-
-/* 
-
-<button
-        onClick={() => signOut()}
-        className='hover:border-gray-500  font-bold py-2 px-4 rounded-full text-red-500 inline-flex items-center space-x-2'>
-        <ArrowLeftOnRectangleIcon className='h-5 w-5' />
-        <span>Sign Out</span>
-      </button>
-
-
-      <button
-        onClick={() => signIn()}
-        className='hover:border-gray-500  font-bold py-2 px-4 rounded-full text-green-500 inline-flex items-center space-x-2'>
-        <ArrowRightOnRectangleIcon className='h-5 w-5' />
-        <span>Sign In</span>
-      </button>
-
-*/
