6965abc2c4948b7a342f92f6814f32627df7dc20
Andres Parra <andeveling@gmail.com>
2022-12-12 18:32:02 -0500

    fix/read-files

--- arqustik.config.ts
+++ arqustik.config.ts
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const arqustikConfig = {
   STRAPI_SERVER: dev ? 'http://localhost:1337/api' : 'https://arqustik-back-production.up.railway.app/api',
-  NEXT_SERVER: dev ? 'http://localhost:3000/api' : 'https://arqustikquoter.vercel.app/',
+  NEXT_SERVER: dev ? 'http://localhost:3000/api' : 'https://arqustikquoter.vercel.app/api',
 }
 
 export const endpoints = {
